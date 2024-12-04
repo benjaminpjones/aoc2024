777e85c3ba35a12d1666ba0f4194c3eb1926e015
Benjamin P. Jones <benjaminpaikjones@gmail.com>
2024-12-03 21:50:11 -0500

    Day 3 - whoops I forgot to save part 1

---
+++ soln3.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <regex>
+
+using namespace std;
+
+bool doMul = true;
+
+long parseAllMul(const string& line) {
+    regex mul_regex("do(n't)?\\(\\)|mul\\(([0-9]+),([0-9]+)\\)");
+
+    auto begin = sregex_iterator(line.begin(), line.end(), mul_regex);
+    auto end = sregex_iterator();
+
+    long sum = 0;
+
+    for (auto it = begin; it != end; ++it) {
+        auto m = *it;
+        if (m.str() == "do()") {
+            doMul = true;
+        } else if (m.str() == "don't()") {
+            doMul = false;
+        } else if (doMul) {
+            sum += stoi(m[2]) * stoi(m[3]);
+        }
+    }
+    return sum;
+}
+
+int main() {
+    string line;
+    long sum = 0;
+    while (getline(cin, line)) {
+        sum += parseAllMul(line);
+    }
+
+    cout << sum << endl;
+
+    return 0;
+}
